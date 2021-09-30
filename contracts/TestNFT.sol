3a485335e43b507c36790b38b6143607ceb71421
Feride3d <80424026+Feride3d@users.noreply.github.com>
2021-09-30 22:08:03 +0300

    Update TestNFT.sol
    Delete SafeMath;
    delete if;
    delete "public" in constructor;
    change TotalSupply;
    change require ("ContractName: ");
    change the format of functions, variables (on CamelCase style).

--- contracts/TestNFT.sol
+++ contracts/TestNFT.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
@@ -12,42 +11,39 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @dev ERC721 Non-Fungible Token Standard basic implementation
  */
 contract TestNFT is ERC721, Ownable {
-    using SafeMath for uint256;
     
     string public provenance = "The first TNFT ever";
     uint256 public constant tnftPrice = 10000000000000000; // 0.01 ETH
-    uint256 public MaxAmountTnft = 1000;
+    uint256 public maxAmountTnft = 1000;
     uint public constant maxTnft = 20;
     bool public saleIsActive = false;
     
 
-    constructor() public ERC721("Test NFT", "TNFT") {
+    constructor() ERC721("Test NFT", "TNFT") {
     }
     
-    function mintTNFT (uint numberOfTokens) public payable {
-        require(saleIsActive, "Sale is active to mint TNFT");
-        require(numberOfTokens <= maxTnft, "Can mint only 20 tokens at the same time");
-        require(totalSupply().add(numberOfTokens) <= MaxAmountTnft, "Max supply of tokens");
-        require(tnftPrice.mul(numberOfTokens) <= msg.value, "Not enough ether");
+    function mintTnft (uint _numberOfTokens) public payable {
+        require(saleIsActive, "TestNFT: Sale is active to mint TNFT");
+        require(_numberOfTokens <= maxTnft, "TestNFT: Can mint only 20 tokens at the same time");
+        require(totalSupply().add(_numberOfTokens) <= maxAmountTnft, "TestNFT: Max supply of tokens");
+        require(tnftPrice.mul(_numberOfTokens) <= msg.value, "TestNFT: Not enough ether");
         
-        for(uint i = 0; i < numberOfTokens; i++) {
-            uint mintIndex = totalSupply();
-            if (totalSupply() < MaxAmountTnft) {
-                _safeMint(msg.sender, mintIndex);
+        for(uint i = 0; i < _numberOfTokens; i++) {
+            uint _mintIndex = totalSupply(); // Из-за конструкции ID токена = totalSupply у первого токена будет индекс 0, что не очень хорошая практика — А какая практика хорошая? Можно записать как mintIndex + 1? 
+                _safeMint(msg.sender, _mintIndex);
             }
         }
     }
     
-    function setProvenance(string memory prov) public onlyOwner {
+    function setProvenance(string memory _prov) public onlyOwner {
         provenance = prov;
     }
     
-    function setBaseURI(string memory baseURI) public onlyOwner {
+    function setBaseURI(string memory _baseURI) public onlyOwner {
         _setBaseURI(baseURI);
     }
     
-    function changeTnftPrice(uint256 memory newTnftPrice) public onlyOwner {  //посмотреть допустимо ли константу (первоначальная цена) менять в таких случаях
-        tnftPrice = newTnftPrice;
-        
+    function changeTnftPrice(uint256 memory _newTnftPrice) public onlyOwner { 
+        tnftPrice = newTnftPrice;    
     }
 }
