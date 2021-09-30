a78cfcbdc07f45900d52cd7217b4c8d68a6c6617
Feride3d <80424026+Feride3d@users.noreply.github.com>
2021-09-30 21:56:59 +0300

    Add files via upload
    Test.js for Hardhat

---
+++ tests/TNFT_test.js
@@ -0,0 +1,70 @@
+const {expectRevert} = require('@openzeppelin/test-helpers');
+const { expect } = require("chai");
+
+ // `beforeEach` will run before each test, re-deploying the contract every
+ // time. It receives a callback, which can be async.
+  beforeEach(async function () {
+
+describe("Test NFT", function () {
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("TNFT");
+
+    const hardhatToken = await Token.deploy();
+
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+});
+
+  
+    // Get the ContractFactory and Signers here.
+    Token = await ethers.getContractFactory("Token");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+
+    // To deploy our contract, we just have to call Token.deploy() and await
+    // for it to be deployed(), which happens once its transaction has been
+    // mined.
+
+    hardhatToken = await Token.deploy();
+  });
+
+  // You can nest describe calls to create subsections.
+  describe("Deployment", function () {
+    // `it` is another Mocha function. This is the one you use to define your
+    // tests. It receives the test name, and a callback function.
+
+    // If the callback function is async, Mocha will `await` it.
+    it("Should set the right owner", async function () {
+      // Expect receives a value, and wraps it in an Assertion object. These
+      // objects have a lot of utility methods to assert values.
+
+      // This test expects the owner variable stored in the contract to be equal
+      // to our Signer's owner.
+      expect(await hardhatToken.owner()).to.equal(owner.address);
+    });
+
+    it("Should assign the total supply of tokens to the owner", async function () {
+      const ownerBalance = await hardhatToken.balanceOf(owner.address);
+      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+
+  describe("Transactions", function () {
+    it("Should transfer tokens between accounts", async function () {
+
+
+      // Check balances.
+      const finalOwnerBalance = await hardhatToken.balanceOf(owner.address);
+      expect(finalOwnerBalance).to.equal(initialOwnerBalance - 150);
+
+      const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(100);
+
+      const addr2Balance = await hardhatToken.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(50);
+    });
+  });
+});
