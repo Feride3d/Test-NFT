3214a6041aec7ab51d5f91158755132faef4d9b9
Feride3d <80424026+Feride3d@users.noreply.github.com>
2021-10-06 00:31:43 +0300

    Add files via upload

--- contracts/TestNFT.sol
+++ contracts/TestNFT.sol
@@ -13,37 +13,36 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract TestNFT is ERC721, Ownable {
     
     string public provenance = "The first TNFT ever";
-    uint256 public constant tnftPrice = 10000000000000000; // 0.01 ETH
-    uint256 public maxAmountTnft = 1000;
-    uint public constant maxTnft = 20;
+    uint256 public TokensPrice = 10000000000000000; // 0.01 ETH
+    uint256 public maxAmountTokens = 1000;
+    uint public constant maxTokens = 20;
     bool public saleIsActive = false;
     
 
     constructor() ERC721("Test NFT", "TNFT") {
     }
     
-    function mintTnft (uint _numberOfTokens) public payable {
+    function mintTokens (uint _numberOfTokens) public payable {
         require(saleIsActive, "TestNFT: Sale is active to mint TNFT");
-        require(_numberOfTokens <= maxTnft, "TestNFT: Can mint only 20 tokens at the same time");
-        require(totalSupply().add(_numberOfTokens) <= maxAmountTnft, "TestNFT: Max supply of tokens");
-        require(tnftPrice.mul(_numberOfTokens) <= msg.value, "TestNFT: Not enough ether");
-        
+        require(_numberOfTokens <= maxTokens, "TestNFT: Can mint only 20 tokens at the same time");
+        require(totalSupply().add(_numberOfTokens) <= maxAmountTokens, "TestNFT: Max supply of tokens");
+        uint256 _lastTokenId = totalSupply();
+
         for(uint i = 0; i < _numberOfTokens; i++) {
-            uint _mintIndex = totalSupply(); // Из-за конструкции ID токена = totalSupply у первого токена будет индекс 0, что не очень хорошая практика — А какая практика хорошая? Можно записать как mintIndex + 1? 
-                _safeMint(msg.sender, _mintIndex);
+            _lastTokenId ++;
+            _mint(_to, _lastTokenId);
             }
         }
     }
     
     function setProvenance(string memory _prov) public onlyOwner {
         provenance = prov;
-    }
+        }
     
     function setBaseURI(string memory _baseURI) public onlyOwner {
         _setBaseURI(baseURI);
-    }
+        }
     
-    function changeTnftPrice(uint256 memory _newTnftPrice) public onlyOwner { 
-        tnftPrice = newTnftPrice;    
-    }
-}
+    function changeTokensPrice(uint256 memory _newTokensPrice) public onlyOwner { 
+        TokensPrice = newTokensPrice;    
+        }
